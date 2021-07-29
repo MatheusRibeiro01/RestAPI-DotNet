b5935636aa9492075c3204fab917587e3b809b28
Matheus Ribeiro <matheusgomes612@gmail.com>
2021-07-29 15:59:21 -0300

    Implementando verbos http

---
+++ Service/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using RestAPI_DotNet.Model;
+
+namespace RestAPI_DotNet.Service.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
